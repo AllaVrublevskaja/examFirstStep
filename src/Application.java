36a164408fe2ed6a241950266c3b7c6f83362e1d
AllaVrublevskaja <allavrublevskaja@yandex.ru>
2023-01-24 14:34:00 +0300

    Добавлен метод MenuPunkt2

--- src/Application.java
+++ src/Application.java
@@ -4,9 +4,14 @@ import model.dao.EmployeeDao;
 import model.dao.LoginDao;
 import model.dao.ProfessionDao;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Application {
     private final LoginDao loginDao;
@@ -20,6 +25,8 @@ public class Application {
     private final Scanner scanner;
     private boolean running;
     private String currentUser;
+    List <Employee> findEmployee;
+    String punktName;
 
     public Application(LoginDao loginDao,DepartmentDao departmentDao,
                        ProfessionDao professionDao,EmployeeDao employeeDao) {
@@ -77,6 +84,7 @@ public class Application {
 
         }
         else if(action == 2) {
+            menuPunkt2();
 
         }
         else if(action == 3) {
@@ -90,7 +98,6 @@ public class Application {
             System.out.println("Неизвестная команда...");
         }
     }
-
     private void registerHandler() {
         System.out.println("Введите Login:");
         String nickName = scanner.next();
@@ -118,6 +125,72 @@ public class Application {
         }
         else {
             System.out.println("Неверный логин или пароль!");
+        }
+    }
+    public void menuPunkt2(){
+        int punkt;
+        boolean quit = false;
+        punkt=5;
+        while(true){
+            System.out.println("Найти сотрудника");
+
+            System.out.println("1. по фамилии");
+            System.out.println("2. по должности");
+            System.out.println("3. по названию отдела");
+            System.out.println("4. по фамилии начальника");
+            System.out.println("5. Выход");
+
+            System.out.println("Введите пункт меню:");
+            if (scanner.hasNextInt())
+                punkt = scanner.nextInt();
+            else {
+                System.out.println("Ошибка");
+                break;
+            }
+
+            switch (punkt){
+                case 1:
+                    punktName = "по фамилии";
+                    System.out.println("Введите Фамилию");
+                    findEmployee(findEmployee);
+                    break;
+                case 2:
+                    punktName = "по должности";
+                    break;
+                case 3:
+                    punktName = "по названию отдела";
+                    break;
+                case 4:
+                    punktName = "по фамилии начальника";
+                    break;
+                case 5:
+                    quit=true;
+                    break;
+            }
+            if (quit) break;
+        }
+    }
+    public void findEmployee(List <Employee> findEmployee,Predicate<Employee> predicate){
+
+        String str = scanner.nextLine();
+        findEmployee=data.stream()
+//                .filter(arr->arr.getSurname().equals(str))
+                .filter(predicate)
+                .collect(Collectors.toList());
+        findEmployee.stream()
+                .forEach(System.out::println);
+        String filename="writeReports\\findEmployeeSurname.txt";
+        writeFindEmployee(filename,str);
+    }
+    public void writeFindEmployee(String filename,String str){
+        try(FileOutputStream fos = new FileOutputStream(filename);
+            PrintWriter pw = new PrintWriter(fos)){
+            pw.println("Список сотрудников "+punktName+": "+str);
+            for (Employee employee : findEmployee)
+                pw.println(employee);
+        }
+        catch(IOException ex){
+            System.out.println("Нет сотрудников "+punktName+": "+str);
         }
     }
 }
